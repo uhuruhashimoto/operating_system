1cabe6b33b7d0f1bb5f1780d8faf3885dfe81d23
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2022-10-15 00:27:57 -0400

    updated region 0 page table; left region 1 page table to be allocated after vmem init (in pcb), also need setbrk

--- checkpoint_1/kernel_start.c
+++ checkpoint_1/kernel_start.c
@@ -1,14 +1,23 @@
 // Virtual Memory specifics
 /*
-* Our system uses two page tables, one in region 0 (kernel access) and one in region 1 (user access).
-* To initialize our page tables, we create a page table in a region of memory such that the physical
-* address and the virtual address are identical; that is, some offset from PMEM_BASE.
+* Our system uses two page tables, one in region 0 (kernel access) and one in region 1 (user access) to give 
+* the kernel the illusion that each process has the full amount of VIRTUAL memory. 
 * 
-* We store our page tables as globals (above the stack) and update the registers that tell the MMU 
-* where to find them. We also keep track of a frame table - a bit vector that tracks which frames are mapped.
+* Region 0 Page Table
+* To initialize our Region 0 page tale, we create a page table in a region of memory such that the physical
+* address and the virtual address are identical; that is, we map pages to frames of the same address. This page 
+* table is of fixed size, and lives in the kernel heap.
+* 
+* Region 1 page table
+* The region 1 page table is the place that individual process page tables are stored. Every process PCB will contain
+* or point to the process page table, and these pointers will be written to the registers that tell the MMU where the 
+* region 1 page table is when the process is run (and the TLB will be flushed). 
+*
+* We use the provided pmem size to keep track of a frame table - a bit vector that tracks which frames are mapped.
 * (See section 2.2.6, p. 22 of Yalnix Manual)
 */
 
+#include <ykernel.h>
 #include "kernel_start.h"
 #include "trap_handlers/trap_handlers.h"
 #include "data_structures/pcb.h"
@@ -18,11 +27,6 @@ extern void *_kernel_data_start;
 extern void *_kernel_data_end;
 extern void *_kernel_orig_brk;
 
-pte_t *region_0_page_table;
-pte_t *region_1_page_table;
-unsigned char *frame_table; 
-bool vmem_enabled = False;
-
 /*
  * Behavior:
  *  Set up virtual memory
@@ -30,46 +34,30 @@ bool vmem_enabled = False;
  *  Instantiate an idlePCB
  */
 void KernelStart(char *cmd args[], unsigned int pmem_size, UserContext *uctxt) {
-  // define memory geometry, adding an extra page if necessary to map entire region
-  int page_table_size = UP_TO_PAGE(pmem_size) << PAGESHIFT;
-  int beginning_page = UP_TO_PAGE(PMEM_BASE) << PAGESHIFT;
-  int kernel_text_end_page = UP_TO_PAGE(_kernel_orig_brk) << PAGESHIFT;
-  int first_user_page = DOWN_TO_PAGE(&region_1_page_table) << PAGESHIFT;
-  int new_brk_page;
   pte_t kernel_page;
-  pte_t user_page;
-
-  //allocate structures and update brk
-  region_0_page_table = malloc(sizeof(pte_t) * page_table_size);
-  region_1_page_table  = malloc(sizeof(pte_t) * page_table_size);
-
-  // set the brk, making the assumption that both page tables are allocated on the heap, which
-  // grows upwards. Thus, the top would be at the end of the most recently allocated structure.
-  if (SetKernelBrk(&region_1_page_table[page_table_size - 1]) == ERROR) {
-    TracePrint("Error moving the brk to %p\n", &region_1_page_table[page_table_size - 1]);
-    exit(ERROR);
-  }
-
-  //Define kernel text permissions as R-X, and heap as RW.
-  new_brk_page = DOWN_TO_PAGE(&region_1_page_table[page_table_size - 1]) << PAGESHIFT;
-  for (int page_ind = 0; page_ind < new_brk_page; page_ind++) {
+  // We use macros to calculate page table size, and provided memory size to calculate frame table size
+  pte_t *region_0_page_table = malloc(sizeof(pte_t) * UP_TO_PAGE(VMEM_0_SIZE)));
+  pte_t *region_1_page_table = malloc(sizeof(pte_t) * UP_TO_PAGE(VMEM_1_SIZE));
+  int *frame_table = malloc(sizeof(int) * UP_TO_PAGE(pmem_size) / PAGESIZE );
+  
+  /*
+  To map the kernel memory into its region 0 page table, we break 
+  its text and heap into PAGESIZE-length chunks, and put them
+  into the page table with self-referential addresses.
+  */
+  int beginning_page = UP_TO_PAGE(PMEM_BASE) << PAGESHIFT;
+  for (int page_ind = 0; page_ind < _kernel_orig_brk; page_ind++) {
       kernel_page -> valid = 1;
       frame_table[beginning_page + page_index] = 1;
       kernel_page -> prot = (page_ind < kernel_text_end_page) ? (PROT_READ | PROT_EXE) : (PROT_READ | PROT_WRITE);
       kernel_page -> pfn = &region_0_page_table[page_ind];
   }
 
-  // write a single page to the page table
-  user_page -> valid = 1;
-  frame_table[first_user_page] = 1;
-  user_page -> prot = (PROT_READ | PROT_WRITE);
-  user_page -> pfn = &region_1_page_table[first_user_page];
+  //TODO: do we need to write the stack and globals too? I'm not sure; up to the brk might be fine.
 
   // update registers
   WriteRegister(REG_PTRBR0, &page_table_reg_0);
   WriteRegister(REG_PTLR0, num_kernel_pages);
-  WriteRegister(REG_PTRBR1, &page_table_reg_1);
-  WriteRegister(REG_PTLR1, 1);
 
   // turn on virtual memory permanently
   WriteRegister(REG_VM_ENABLE, 1);
@@ -93,8 +81,9 @@ void KernelStart(char *cmd args[], unsigned int pmem_size, UserContext *uctxt) {
 
   // TODO -- allocate memory for the ready queue
 
-  // TODO -- IDLE PCB
-  // TODO -- stick the idle PCB into the running_process global
+  //TODO: create idle PCB and its page table, and write it to the correct registers (and store its PCB, and
+  //page table, in the heap)
+  // Then stick the idle PCB into the running_process global
 
 }
 
