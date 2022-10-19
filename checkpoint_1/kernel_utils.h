ff4967e2653d599d4f7cd51c27b11032ae115daf
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2022-10-18 23:05:31 -0400

    not sure what I changed but smth

---
+++ checkpoint_1/kernel_utils.h
@@ -0,0 +1,14 @@
+#include <ykernel.h>
+
+/*
+* Switches kernel context, being careful to save special registers
+*/
+KernelContext *KCSwitch( KernelContext *kc_in, void *curr_pcb_p, void *next_pcb_p);
+
+/*
+* Copies the kernel context from *kc_in to the *new_pcb_p, and copies
+* kernel stack to unused, allocated frames. Returns kc_in.
+*/
+KernelContext *KCCopy( KernelContext *kc_in, void *new_pcb_p,void *not_used);
+
+KernelContext *SetKernelBrk();
