804981f5ef7f4053e2ae2814ec10c32e290b4e67
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2022-10-25 23:27:36 -0400

    breakout frame table

---
+++ checkpoint_1/data_structures/frame_table.h
@@ -0,0 +1,24 @@
+#ifndef CURRENT_CHUNGUS_FRAME_TABLE_H
+#define CURRENT_CHUNGUS_FRAME_TABLE_H
+
+#include <ykernel.h>
+#define MEMFULL -1
+
+typedef struct frame_table_struct{
+  char *frame_table;
+  int frame_table_size;
+} frame_table_struct_t;
+
+//=================== FRAME TABLE FUNCTIONS =================//
+
+/*
+* return the number of the next free frame, or MEMFULL if memory is insufficient
+*/
+int get_free_frame(char *frame_table, int frame_table_size);
+
+/*
+* return the number of free frames
+*/
+int get_num_free_frames(char *frame_table, int frame_table_size);
+
+#endif //CURRENT_CHUNGUS_FRAME_TABLE_H
