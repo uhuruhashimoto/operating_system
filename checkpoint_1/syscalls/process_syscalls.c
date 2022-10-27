9e9cb975b58a7cd51e960ff92fdc081084ff21b9
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2022-10-27 18:06:58 -0400

    setbrk

--- checkpoint_1/syscalls/process_syscalls.c
+++ checkpoint_1/syscalls/process_syscalls.c
@@ -1,4 +1,16 @@
 #include <ykernel.h>
+#include "../kernel_start.h"
+#include "../data_structures/pcb.h"
+#include "../data_structures/queue.h"
+#include "../data_structures/frame_table.h"
+
+extern frame_table_struct_t *frame_table_global;
+extern pcb_t* running_process;
+extern pcb_t* idle_process;                                           // the special idle process; use when nothing is in ready queue
+extern bool is_idle;                                                  // if is_idle, we won't put the process back on the ready queue
+extern queue_t* ready_queue;
+extern void *trap_handler[16];
+extern pte_t *region_0_page_table;
 /*
  * Fork the process and create a new, separate address space
  */
@@ -70,13 +82,70 @@ int handle_GetPid(void)
  */
 int handle_Brk(void *addr)
 {
-  // Rounds the brk up to the next page
-  // if increasing the size of the brk:
-  //  create valid pages in the page table for all new memory below the brk
-  // else:
-  //  invalidates pages on the page table above the brk
-  // sets the brk on the PCB
-  // ERROR on failure, 0 on success
+  //error handling
+  if (addr < VMEM_1_BASE) {
+    TracePrintf(1, "Error: New brk address provided is in Region 0 (kernel memory).\n");
+    return ERROR;
+  }
+  else if (addr > VMEM_1_LIMIT) {
+    TracePrintf(1, "Error: New brk address provided is above writable memory.\n");
+    return ERROR;
+  } 
+  
+  pte_t *region_1_page_table = running_process->region_1_page_table;
+  int addr_page = addr >> PAGESHIFT;
+  int current_brk_page = 0;
+  int region_1_page_table_size = VMEM_1_SIZE >> PAGESHIFT;
+  bool brkfound = false;
+
+  //find the brk
+  while (!brkfound && current_brk_page < region_1_page_table_size){
+    if (!region_1_page_table[current_brk_page].valid) {
+      brkfound = true;
+    }
+    current_brk_page++;
+  }
+  TracePrintf("SETBRK: Current brk found at %d pages\n", current_brk_page);
+
+  int first_possible_free_frame = 0;
+  if (addr_page > current_brk_page) {
+    TracePrintf(3, "SETBRK: Will try to find memory to allocate more frames\n");
+    // error out if we don't have enough memory
+    if (addr_page-current_brk_page > get_num_free_frames(frame_table_global->frame_table,
+                                                                  frame_table_global->frame_table_size)
+        ) {
+      TracePrintf(1, "SETBRK: SetKernelBrk did not find enough memory for the whole malloc to succeed\n");
+      return ERROR;
+    }
+
+    TracePrintf(3, "SETBRK: SetKernelBrk found enough memory for malloc to succeed\n");
+    while (addr_page > current_brk_page) {
+      int new_frame_num = get_free_frame(frame_table_global->frame_table,
+                                          frame_table_global->frame_table_size,
+                                          first_possible_free_frame);
+      if (new_frame_num == -1) {
+        TracePrintf(1, "SETBRK: SetKernelBrk was unable to allocate a new frame...\n");
+        return ERROR;
+      }
+      region_1_page_table[current_brk_page].valid = 1;
+      region_1_page_table[current_brk_page].prot = (PROT_READ | PROT_WRITE);
+      region_1_page_table[current_brk_page].pfn = new_frame_num;
+      current_brk_page++;
+    }
+    return SUCCESS;
+  }
+  else {
+    // TODO -- does this code path ever execute?
+    TracePrintf(3, "SETBRK: SetKernelBrk found that we don't need to allocate more frames\n");
+    while (addr_page < current_brk_page) {
+      TracePrintf(3, "SETBRK: Deleting a page from the page table...\n");
+      int discard_frame_number = region_1_page_table[current_brk_page].pfn;
+      frame_table_global->frame_table[discard_frame_number] = 0;
+      region_1_page_table[current_brk_page].valid = 0;
+      current_brk_page--;
+    }
+    return SUCCESS;
+  }
 }
 
 /*
