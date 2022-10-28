29eb3422ad68edc19a3ce1814859efe92169c3a1
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2022-10-28 17:10:53 -0400

    debug utilities

--- checkpoint_1/debug_utils/debug.c
+++ checkpoint_1/debug_utils/debug.c
@@ -1,5 +1,5 @@
 #include <ykernel.h>
-#include "kernel_start.h"
+#include "../kernel_start.h"
 #include "../data_structures/pcb.h"
 #include "../data_structures/queue.h"
 #include "../data_structures/frame_table.h"
@@ -12,22 +12,24 @@ extern queue_t* ready_queue;
 extern void *trap_handler[16];
 extern pte_t *region_0_page_table;
 
-//reg 0, reg 1, kernel stack
-int print_reg_0_page_table_global(int level) {
-    int region_0_page_table_size = VMEM_0_SIZE << PAGESHIFT; 
-    TracePrintf(level, "=====Region 0 Page Table (%d pages)=====\n", region_1_page_table_size);
+// print current page table global
+void print_reg_0_page_table(int level) {
+    int region_0_page_table_size = UP_TO_PAGE(VMEM_0_SIZE) >> PAGESHIFT; 
+    TracePrintf(level, "=====Region 0 Page Table (%d pages)=====\n", region_0_page_table_size);
     for (int i = 0; i < region_0_page_table_size; i++) {
-    if (region_0_page_table[i].valid) {
-        TracePrintf(level, "Addr: %x to %x, Valid: %d, Pfn: %d\n",
-                    VMEM_0_BASE + (i << PAGESHIFT),
-                    VMEM_0_BASE + ((i+1) << PAGESHIFT)-1,
-                    region_0_page_table[i].valid,
-                    region_0_page_table[i].pfn
-        );
+        if (region_0_page_table[i].valid) {
+            TracePrintf(level, "Addr: %x to %x, Valid: %d, Pfn: %d\n",
+                        VMEM_0_BASE + (i << PAGESHIFT),
+                        VMEM_0_BASE + ((i+1) << PAGESHIFT)-1,
+                        region_0_page_table[i].valid,
+                        region_0_page_table[i].pfn
+            );
+        }
     }
 }
 
-int print_kernel_stack(int level) {
+// print current kernel stack (from page table)
+void print_kernel_stack(int level) {
     int stack_start_page = UP_TO_PAGE(KERNEL_STACK_BASE - PMEM_BASE) >> PAGESHIFT;
     int stack_end_page = UP_TO_PAGE(KERNEL_STACK_LIMIT - PMEM_BASE) >> PAGESHIFT;
 
@@ -43,23 +45,26 @@ int print_kernel_stack(int level) {
     
 }
 
-int print_reg_1_page_table(pcb_t *process, int level) {
+// print region 1 page table for a given process
+void print_reg_1_page_table(pcb_t *process, int level) {
     pte_t *region_1_page_table = process->region_1_page_table;
-    int region_1_page_table_size = VMEM_1_SIZE << PAGESHIFT;
+    int region_1_page_table_size = UP_TO_PAGE(VMEM_1_SIZE) >> PAGESHIFT;
     TracePrintf(level, "=====Region 1 Page Table for pid %d (%d pages)=====\n", process->pid, region_1_page_table_size);
     for (int i = 0; i < region_1_page_table_size; i++) {
-    if (region_1_page_table[i].valid) {
-        TracePrintf(5, "Addr: %x to %x, Valid: %d, Pfn: %d\n",
-                    VMEM_1_BASE + (i << PAGESHIFT),
-                    VMEM_1_BASE + ((i+1) << PAGESHIFT)-1,
-                    region_1_page_table[i].valid,
-                    region_1_page_table[i].pfn
-        );
-    } 
+        if (region_1_page_table[i].valid) {
+            TracePrintf(level, "Addr: %x to %x, Valid: %d, Pfn: %d\n",
+                        VMEM_1_BASE + (i << PAGESHIFT),
+                        VMEM_1_BASE + ((i+1) << PAGESHIFT)-1,
+                        region_1_page_table[i].valid,
+                        region_1_page_table[i].pfn
+            );
+        } 
+    }
 }
 
-int print_frame_table(int level) {
-    for (int i=0; i<frame_table_global->size; i++) {
-        TracePrintf(level, "Frame %d: %d\n", i, frame_table[i]);
+// print the frame table (all frames)
+void print_frame_table(int level) {
+    for (int i=0; i<frame_table_global->frame_table_size; i++) {
+        TracePrintf(level, "Frame %d: %d\n", i, frame_table_global[i]);
     }
 }
--- checkpoint_1/debug_utils/debug.h
+++ checkpoint_1/debug_utils/debug.h
@@ -1,3 +1,6 @@
+#ifndef CURRENT_CHUNGUS_DEBUG_UTILS
+#define CURRENT_CHUNGUS_DEBUG_UTILS 
+
 #include <ykernel.h>
 #include "../kernel_start.h"
 #include "../data_structures/pcb.h"
@@ -12,10 +15,16 @@ extern queue_t* ready_queue;
 extern void *trap_handler[16];
 extern pte_t *region_0_page_table;
 
-int print_reg_0_page_table_global(int level);
+// print current page table global
+void print_reg_0_page_table(int level);
 
-int print_kernel_stack(int level);
+// print current kernel stack (from page table)
+void print_kernel_stack(int level);
 
-int print_reg_1_page_table(pcb_t *process, int level);
+// print region 1 page table for a given process
+void print_reg_1_page_table(pcb_t *process, int level);
 
-int print_frame_table(int level);
+// print the frame table (all frames)
+void print_frame_table(int level); 
+
+#endif
