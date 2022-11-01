db20050f2c6013d846aa4d01a1c5a22f902f12c9
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2022-10-31 22:53:12 -0400

    fork/exec now working until end of call, when exec segfaults (page table problem maybe)

--- checkpoint_1/test_processes/exec_test.c
+++ checkpoint_1/test_processes/exec_test.c
@@ -4,11 +4,12 @@
 int main(const int argc, char **argv) {
   int pid = Fork();
     if (pid == 0) {
-        Exec("exec_test", argv);
+        Exec("checkpoint_1/test_processes/exec_test", argv);
         TracePrintf(1, "Exec failed\n");
     }
   // do nothing
   while (1) {
-
+    TracePrintf(1, "I'm the parent w pid %d, looping forever\n", pid);
+    Pause();
   }
 }
