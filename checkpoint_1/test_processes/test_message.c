ffe35f98a334f18af4552d3ebd08913a8e57de8d
Elliot Potter <58124535+averageflamethrowerguy@users.noreply.github.com>
2022-11-03 21:33:16 -0400

    print argc as well

--- checkpoint_1/test_processes/test_message.c
+++ checkpoint_1/test_processes/test_message.c
@@ -7,6 +7,8 @@
 int main(const int argc, char **argv) {
   TracePrintf(1, "Test message program\n");
 
+  TracePrintf(1, "Argc: %d\n", argc);
+
   for (int i = 0; argv[i] != NULL; i++) {
     TracePrintf(1, "arg = '%s'\n", argv[i]);
   }
