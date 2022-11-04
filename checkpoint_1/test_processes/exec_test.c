84671517c34d01a4fe2ac8e5a2a6fc6af363c5d4
Elliot Potter <58124535+averageflamethrowerguy@users.noreply.github.com>
2022-11-03 21:20:19 -0400

    Maybe a fix for args

--- checkpoint_1/test_processes/exec_test.c
+++ checkpoint_1/test_processes/exec_test.c
@@ -2,6 +2,12 @@
 #define PAGESIZE 0x2000
 
 int main(const int argc, char **argv) {
-    Exec("checkpoint_1/test_processes/test_message", argv);
+  char** argv_temp = malloc(sizeof (char* ) * 5);
+  argv_temp[0] = "FOO";
+  argv_temp[1] = "BAR";
+  argv_temp[2] = "BIZ";
+  argv_temp[3] = "BAZ";
+  argv_temp[4] = NULL;
+    Exec("checkpoint_1/test_processes/test_message", argv_temp);
     TracePrintf(1, "Exec failed\n");
 }
--- checkpoint_1/test_processes/test_message.c
+++ checkpoint_1/test_processes/test_message.c
@@ -5,6 +5,12 @@
 #define PAGESIZE 0x2000
 
 int main(const int argc, char **argv) {
+  TracePrintf(1, "Test message program\n");
+
+  for (int i = 0; argv[i] != NULL; i++) {
+    TracePrintf(1, "arg = '%s'\n", argv[i]);
+  }
+
     for (int i = 0; i<10; i++) {
         TracePrintf(1, "AAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
     }
