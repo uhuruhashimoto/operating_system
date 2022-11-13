20e346d7da575d29b7049813a9c788eb5dc471c7
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2022-11-13 12:47:26 -0500

    updated fork bomb test

--- checkpoint_1/test_processes/fork_exec_wait_tests/fork_bomb.c
+++ checkpoint_1/test_processes/fork_exec_wait_tests/fork_bomb.c
@@ -5,12 +5,13 @@ int main(void) {
         int pid = Fork();
         TracePrintf(1, "Fork returned %d\n", pid);
         if (pid == 0) {
-            TracePrintf(1, "Child back from fork!\n", pid);
+            int child_pid = GetPid();
+            TracePrintf(1, "Child %d back from fork!\n", child_pid);
             while(1) {
                 Pause();
-                TracePrintf(1, "Hi! I'm the child and I'll run forever.\n");
+                TracePrintf(1, "Hi! I'm the child with pid %d and I'll run forever.\n", child_pid);
             }
         }
-        TracePrintf(1, "Parent back from fork!\n", pid);
+        TracePrintf(1, "Parent %d back from fork!\n", pid);
     }
 }
